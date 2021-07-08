3dbb809388b5bff1bc710f22988ad3c13b45b6bb
Kygekraqmak <techfixindonesia@gmail.com>
2021-07-09 07:56:16 +0800

    Added license notice

--- src/KygekDev/IsOP/IsOP.php
+++ src/KygekDev/IsOP/IsOP.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * List OP players and check if player is OP using command
+ * Copyright (C) 2021 KygekDev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 declare(strict_types=1);
 
 namespace KygekDev\IsOP;
